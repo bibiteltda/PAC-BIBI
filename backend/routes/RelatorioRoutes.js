52e60a2b1d9ba848b15001ca9d6dea8ac31748b7
Gabriel Bandoch <96153388+GabrielBandoch@users.noreply.github.com>
2025-10-12 00:22:36 -0300

    fix: nomenclatura

--- backend/routes/RelatorioRoutes.js
+++ backend/routes/RelatorioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import relatorioController from '../controllers/relatorioController.js';
+import relatorioController from '../controllers/RelatorioController.js';
 
 const router = express.Router();
 
