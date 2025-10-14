73a235c4a799f2f6efdb97456484c618d64742fd
EricSemCDev <165967197+EricSemCDev@users.noreply.github.com>
2025-10-13 22:48:17 -0300

    Update Autenticacao.js

--- backend/api/models/Autenticacao.js
+++ backend/api/models/Autenticacao.js
@@ -1,6 +1,7 @@
 module.exports = {
   tableName: 'Autenticacao',
   primaryKey: 'id_autenticacao',
+  schema: true,
   attributes: {
     id_autenticacao: { type: 'number', autoIncrement: true },
     login: { type: 'string', required: true, maxLength: 150 },
@@ -8,7 +9,5 @@ module.exports = {
     role: { type: 'string', required: true, maxLength: 50 },
     responsaveis: { collection: 'responsavel', via: 'id_autenticacao' }, 
     motoristas: { collection: 'motorista', via: 'id_autenticacao' },
-    
-    schema: true
   }
 };
